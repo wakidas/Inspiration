c8ac57398245af1f19f21707dd41524689638d6f
脇田　慎平 <ymnkknt3@gmail.com>
2020-06-14 10:34:38 +0900

    ロゴ画像設置

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,8 +1,8 @@
 @section('header')
 <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
     <div class="container">
-        <a class="navbar-brand" href="{{ url('/') }}">
-            {{ config('app.name', 'Laravel') }}
+        <a class="" href="{{ url('/') }}">
+            <img src="images/logo.png" alt="Inspiration">
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
