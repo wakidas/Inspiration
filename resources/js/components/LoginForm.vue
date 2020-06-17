570d79ba3b59bdd73ed1efa853ca316f996ef00c
脇田　慎平 <ymnkknt3@gmail.com>
2020-06-18 00:52:37 +0900

    ログイン画面実装 vueコンポーネント化

---
+++ resources/js/components/LoginForm.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="p-login__form">
+    <form method="POST" :action="endpoint">
+      <input type="hidden" name="_token" :value="csrf" />
+
+      <!-- メールアドレス -->
+      <div class="p-login__formGroup">
+        <label for="email" class>メールアドレス</label>
+        <div class="p-login__formItem">
+          <input
+            id="email"
+            type="email"
+            class="p-login__formInput @error('email') is-invalid @enderror"
+            name="email"
+            v-model="email"
+            required
+            autocomplete="email"
+          />
+
+          <span class="c-error" role="alert" v-for="value in error.email" :key="value.email">
+            <strong>{{ value }}</strong>
+          </span>
+        </div>
+      </div>
+
+      <!-- パスワード -->
+      <div class="p-login__formGroup">
+        <label for="password" class>パスワード</label>
+        <div class="p-login__formItem">
+          <input
+            id="password"
+            type="password"
+            class="p-login__formInput @error('password') is-invalid @enderror"
+            name="password"
+            v-model="password"
+            required
+            autocomplete="current-password"
+          />
+          <span class="c-error" role="alert" v-for="value in error.password" :key="value.id">
+            <strong>{{ value }}</strong>
+          </span>
+        </div>
+      </div>
+
+      <!-- ログインしたままにする -->
+      <div class="p-login__formGroup p-login__formGroup--remember">
+        <input
+          class="p-login__formInput p-login__formInput--remember"
+          type="checkbox"
+          name="remember"
+          id="remember"
+        />
+        <label class="p-login__formLabel" for="remember">ログインしたままにする</label>
+      </div>
+
+      <!-- 送信ボタン -->
+      <div class="p-login__submit">
+        <button type="submit" class="c-button__submit">ログインする</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["old", "errors", "endpoint"],
+  data: function() {
+    return {
+      csrf: document
+        .querySelector('meta[name="csrf-token"]')
+        .getAttribute("content"),
+      email: this.old.email,
+      password: "",
+      remember: this.old.remember,
+      error: {
+        email: this.errors.email,
+        password: this.errors.password
+      }
+    };
+  }
+};
+</script>
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,56 +10,10 @@
         <div class="p-login__inner">
             <div class="p-login__title">ログイン</div>
 
-            <div class="p-login__form">
-                <form method="POST" action="{{ route('login') }}">
-                    @csrf
-
-                    <div class="p-login__formGroup">
-                        <label for="email" class="">メールアドレス</label>
-
-                        <div class="p-login__formItem">
-                            <input id="email" type="email"
-                                class="p-login__formInput @error('email') is-invalid @enderror" name="email"
-                                value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                            @error('email')
-                            <span class="" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                            @enderror
-                        </div>
-                    </div>
-
-                    <div class="p-login__formGroup">
-                        <label for="password" class="">パスワード</label>
-
-                        <div class="p-login__formItem">
-                            <input id="password" type="password"
-                                class="p-login__formInput @error('password') is-invalid @enderror" name="password"
-                                required autocomplete="current-password">
-
-                            @error('password')
-                            <span class="" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                            @enderror
-                        </div>
-                    </div>
-
-                    <div class="p-login__formGroup p-login__formGroup--remember">
-                        <input class="p-login__formInput p-login__formInput--remember" type="checkbox" name="remember"
-                            id="remember" {{ old('remember') ? 'checked' : '' }}>
-                        <label class="p-login__formLabel" for="remember">ログインしたままにする</label>
-                        {{-- <p></p> --}}
-                    </div>
-
-                    <div class="p-login__submit">
-                        <button type="submit" class="c-button__submit">
-                            ログインする
-                        </button>
-                    </div>
-                </form>
-            </div>
+            <login-form :old="{{ json_encode(Session::getOldInput()) }}" :errors="{{ $errors }}"
+                endpoint="{{ route('login') }}">
+            </login-form>
         </div>
     </div>
-    @endsection
+</div>
+@endsection
