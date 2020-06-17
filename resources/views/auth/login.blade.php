28da3f657d6c90a54dc9a92f7023c53ea43cef46
脇田　慎平 <ymnkknt3@gmail.com>
2020-06-17 18:48:38 +0900

    ログイン画面見た目ざっくり_PCSP両方

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,65 @@
 @extends('layouts.app')
+@include('layouts.footer')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+<div class="l-login">
+    <div class="p-login">
+        <div class="p-login__logo">
+            <img src="/images/logo.png" alt="Inspiration">
+        </div>
+        <div class="p-login__inner">
+            <div class="p-login__title">ログイン</div>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+            <div class="p-login__form">
+                <form method="POST" action="{{ route('login') }}">
+                    @csrf
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                    <div class="p-login__formGroup">
+                        <label for="email" class="">メールアドレス</label>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                        <div class="p-login__formItem">
+                            <input id="email" type="email"
+                                class="p-login__formInput @error('email') is-invalid @enderror" name="email"
+                                value="{{ old('email') }}" required autocomplete="email" autofocus>
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+                            @error('email')
+                            <span class="" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror
                         </div>
+                    </div>
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                    <div class="p-login__formGroup">
+                        <label for="password" class="">パスワード</label>
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                        <div class="p-login__formItem">
+                            <input id="password" type="password"
+                                class="p-login__formInput @error('password') is-invalid @enderror" name="password"
+                                required autocomplete="current-password">
 
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+                            @error('password')
+                            <span class="" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror
                         </div>
+                    </div>
 
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                    <div class="p-login__formGroup p-login__formGroup--remember">
+                        <input class="p-login__formInput p-login__formInput--remember" type="checkbox" name="remember"
+                            id="remember" {{ old('remember') ? 'checked' : '' }}>
+                        <label class="p-login__formLabel" for="remember">ログインしたままにする</label>
+                        {{-- <p></p> --}}
+                    </div>
 
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
+                    <div class="p-login__submit">
+                        <button type="submit" class="c-button__submit">
+                            ログインする
+                        </button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
-</div>
-@endsection
+    @endsection
