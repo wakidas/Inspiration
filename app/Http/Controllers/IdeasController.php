7124d27f862281ae7104b322786ed49ed16c935a
脇田　慎平 <ymnkknt3@gmail.com>
2020-06-26 01:08:44 +0900

    ideaコントローラーにdocコメントを新規で追加

--- app/Http/Controllers/IdeasController.php
+++ app/Http/Controllers/IdeasController.php
@@ -8,8 +8,19 @@ use inspiration\Idea;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
 
+/**
+ * アイデアのCRUD用コントローラー
+ * 
+ * アイデアのCRUDに関連するメソッド群を記載
+ */
 class IdeasController extends Controller
 {
+    /**
+     * アイデア一覧ページへアクセスする
+     *
+     * @var array $ideas アイデア一覧データ
+     * @return Response アイデア一覧ページの表示
+     */
     public function index()
     {
         $ideas = Idea::all();
@@ -17,6 +28,13 @@ class IdeasController extends Controller
             'ideas' => $ideas,
         ]);
     }
+
+    /**
+     * アイデアの新規投稿画面へアクセス
+     *
+     * @var array $ideas アイデア一覧データ
+     * @return Response アイデア投稿ページの表示
+     */
     public function create()
     {
         $ideas = Idea::all();
@@ -25,6 +43,15 @@ class IdeasController extends Controller
         ]);
     }
 
+    /**
+     * DBへの新規登録メソッド
+     *
+     * @param array $request バリデーション通過したpostの内容
+     * @var object $user ログインユーザー情報
+     * @var object $postImg postされた画像の情報
+     * @var object $idea  newされた新規アイデアのインスタンス
+     * @return Response アイデア一覧ページの表示
+     */
     public function store(Request $request)
     {
         Log::debug('$request');
@@ -38,9 +65,11 @@ class IdeasController extends Controller
 
         if ($postImg) {
             $path = $postImg->store('public/idea_images');
-            $idea->img = str_replace('public/', '', $path);
+            $idea->img = str_replace('public/', 'storage/', $path);
         }
 
         $idea->save();
+
+        return redirect()->route('ideas.index')->with('flash_message', 'アイデアを投稿しました！');
     }
 }
