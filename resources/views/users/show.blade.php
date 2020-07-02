a2e00c5e5268930f8497e2077f26b8d0370694cf
脇田　慎平 <ymnkknt3@gmail.com>
2020-07-02 22:46:00 +0900

    ユーザー詳細ページ　アイテムカードリスト

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -6,17 +6,19 @@
 <div class="l-main">
     <div class="p-usersShow">
         <users-profile :user='@json($this_user)' :is-auth-check='@json($isAuthCheck)'
-        endpoint={{ route('users.edit',$user->id) }}>></users-profile>
-        <div class="p-ideasList">
-            @foreach ($ideas as $idea)
-            <ideas-list :idea='@json($idea)' :user='@json($idea->user)' :category='@json($idea->category)'
-                :likes-count='@json($idea->likesCount)' endpoint='{{route('ideas.show',$idea->id)}}'
-                :avg-rate='@json($idea->reviews->avg("rate"))' :review-count='@json($idea->reviews->count())'
-                >
-            </ideas-list>
+            endpoint={{ route('users.edit',$user->id) }}>></users-profile>
+        <section class="p-ideasList" id="js-ideas-list">
+            <div class="p-ideasList__title">投稿アイデア一覧</div>
+            <div class="p-ideasList__inner">
+                @foreach ($ideas as $idea)
+                <ideas-list :idea='@json($idea)' :user='@json($idea->user)' :category='@json($idea->category)'
+                    :likes-count='@json($idea->likesCount)' endpoint='{{route('ideas.show',$idea->id)}}'
+                    :avg-rate='@json($idea->reviews->avg("rate"))' :review-count='@json($idea->reviews->count())'>
+                </ideas-list>
 
-            @endforeach
-        </div>
+                @endforeach
+            </div>
+        </section>
     </div>
 </div>
 @endsection
