fb5d5099ab1c366215086efc1d6e8d8e60b36dd7
脇田　慎平 <ymnkknt3@gmail.com>
2020-07-11 17:39:40 +0900

    twitterカード画像

--- resources/views/layouts/meta_sns.blade.php
+++ resources/views/layouts/meta_sns.blade.php
@@ -3,6 +3,6 @@
 <meta property="og:description" content="{{ strip_tags($idea->body) }}" />
 
 <meta property="og:site_name" content="Inspiration" />
-<meta property="og:image" content="/images/top_fv.svg" />
+<meta property="og:image" content="http://inspiration.kiwatchi.com/images/top_fv.svg" />
 
 <meta name="twitter:card" content="summary">
